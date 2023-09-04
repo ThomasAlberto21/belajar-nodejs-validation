28c006dc7c231c8b3d853049c265eaeb7c14c972
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-04 10:28:32 +0700

    Validation Error

---
+++ tests/validation-error.test.js
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+describe("Joi", () => {
+  it("should return validation error", () => {
+    const emailSchema = Joi.string().min(5).email().required();
+    const result = emailSchema.validate("ups");
+    console.info(result);
+
+    if (result.error) {
+      result.error.details.forEach((detail) => {
+        console.info(`${detail.path} = ${detail.message}`);
+      });
+    }
+  });
+});
